0a9731ad4709106011cf25782a35b5be957ee515
Darren7749 <83978243+Darren7749@users.noreply.github.com>
2023-02-20 09:47:38 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Darren Page</title>
+    </Head>
+    <h1>Darren</h1>
+    <p>
+    “Death is like the wind – always by my side.”.
+    </p>
+    </>
+  )
+}
